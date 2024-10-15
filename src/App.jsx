79b983acbfa8cae7c01f87684869f5e3e2cf7525
Arafat <arafatahmedshlbd194@gmail.com>
2024-10-15 11:29:20 +0600

    Ui Done

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import NewTodo from "./Components/NewTodo";
 
 const App = () => {
   return (
-    <div className="flex flex-col items-center justify-center text-3xl w-full min-h-screen bg-stone-600 text-white top-1/2">
+    <div className="flex flex-col items-center justify-center text-3xl w-full min-h-screen bg-stone-600 text-white top-1/2 px-2">
       <h2 className="text-white mb-3 font-semibold ">Todo List</h2>
-      <div className="min-w-min w-full max-w-lg flex items-center overflow-y-hidden">
-        <input type="text" placeholder="Write todos" className="focus:outline-none text-xl text-black px-2 py-4 rounded-s w-[90%]"/>
-        <button type="submit" className="bg-orange-900 px-2 py-[14px] rounded-e text-2xl">Add</button>
+      <div className="min-w-min w-full max-w-lg flex items-center justify-center h-[60px]">
+        <input type="text" placeholder="Write todos" className="focus:outline-none text-xl text-black px-2 py-4 rounded-s w-full sm:w-[90%]"/>
+        <button type="submit" className="bg-orange-900 px-2 sm:py-[14px] rounded-e sm:text-2xl texi-xl h-full">Add</button>
       </div>
       <div className="border-black min-w-min w-full max-w-lg">
       <NewTodo />
--- src/Components/NewTodo.jsx
+++ src/Components/NewTodo.jsx
@@ -2,22 +2,22 @@ import React from "react";
 
 const NewTodo = () => {
   return (
-    <div className="bg-slate-600 px-4 py-3 mt-4 flex justify-between items-center rounded-md border-white/10 border-2 gap-5 shadow-lg">
-      <div className="flex justify-center items-center">
+    <div className="bg-slate-600 sm:px-4 sm:py-3 p-1 mt-4 flex justify-around sm:justify-between rounded-md border-white/10 border-2 gap-5 shadow-lg">
+      <div className="flex items-center">
         <input type="checkbox" />
         <input
           type="text"
-          className="bg-transparent border-none min-w-min focus:outline-none text-white text-lg pl-4"
+          className="bg-transparent border-none w-[170px] sm:min-w-min focus:outline-none text-white text-lg sm:pl-4 pl-2"
           disabled
         />
       </div>
-      <div>
+      <div className="flex overflow-hidden">
         <img
-          className="w-9 inline-block mx-2 cursor-pointer border-2 my-1 p-1 h-9 border-yellow-300/30 border-dotted rounded-lg"
+          className="w-7 sm:w-9 mx-2 cursor-pointer border-2  p-1 my-1 sm:h-9 border-yellow-300/30 border-dotted rounded-lg"
           src="./src/assets/pen.svg"
         />
         <img
-          className="w-9 inline-block cursor-pointer border-2 my-1 p-1 h-9 border-yellow-300/30 border-dotted rounded-lg "
+          className="w-7 sm:w-9 cursor-pointer border-2 my-1 p-1 sm:h-9 border-yellow-300/30 border-dotted rounded-lg "
           src="./src/assets/delete.svg"
         />
       </div>
